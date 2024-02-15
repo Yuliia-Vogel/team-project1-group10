d082e1ca88a20fc4fcd561b6503b5220e7c03142
Yuliia-Vogel <arwen.vogel@gmail.com>
2024-02-16 00:08:51 +0200

    The first scratch of the bot.

---
+++ bot-assistant/contacts.py
@@ -0,0 +1,140 @@
+from datetime import datetime
+from collections import UserDict
+import json
+
+class Field:
+    def __init__(self, value):
+        if not self.is_valid(value):
+            raise ValueError("Invalid value")
+        self.__value = value
+
+    def __str__(self):
+        return str(self.__value)
+
+    def is_valid(self, value):
+        return True
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        if not self.is_valid(value):
+            raise ValueError("Invalid value")
+        self.__value = value
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    def is_valid(self, value):
+        return len(value) == 10 and value.isdigit()
+
+    def __init__(self, value):
+        super().__init__(value)
+
+
+
+class Birthday(Field):
+    def __init__(self, value=None):
+        if value:
+            self._validate_birthday_format(value)
+        super().__init__(value)
+
+    def _validate_birthday_format(self, value):
+        if value.lower() != 'none':  
+            try:
+                datetime.strptime(value, '%Y-%m-%d')
+            except ValueError:
+                raise ValueError
+
+    #def __str__(self):
+        #return str(self.value) if self.value else ""  
+
+
+class Record:
+    def __init__(self, name, birthday=None):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = Birthday(birthday)
+
+    def add_phone(self, phone):
+        self.phones.append(str(phone))  
+
+    def remove_phone(self, phone):
+        self.phones.remove(phone)
+
+    def edit_phone(self, old_phone, new_phone):
+        index = self.phones.index(old_phone)
+        self.phones[index] = new_phone
+
+    def find_phone(self, phone):
+        return phone in self.phones
+
+    def days_to_birthday(self):
+        if not self.birthday.value:
+            return None
+        today = datetime.today()
+        next_birthday = datetime(today.year, self.birthday.value.month, self.birthday.value.day)
+        if next_birthday < today:
+            next_birthday = datetime(today.year + 1, self.birthday.value.month, self.birthday.value.day)
+        delta = next_birthday - today
+        return delta.days
+
+    def __str__(self):
+        birthday_info = f", Birthday: {self.birthday}" if self.birthday.value else ""
+        return f"Contact name: {self.name.value}, phones: {'; '.join(self.phones)}{birthday_info}"
+
+
+class AddressBook(UserDict):
+    def __init__(self, filename):
+        self.filename = filename
+        super().__init__()
+        self.load_from_json()
+
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+    def __iter__(self):
+        return self.iterator()
+
+    def iterator(self, part_record=1):
+        records = list(self.data.values())
+        num_records = len(records)
+        current_index = 0
+        while current_index < num_records:
+            yield records[current_index:current_index + part_record]
+            current_index += part_record
+
+    def save_to_json(self):
+        with open(self.filename, "w") as fh:
+            json.dump([{"name": record.name.value,
+                        "phones": record.phones,
+                        "birthday": str(record.birthday)} for record in self.data.values()], fh, indent=4)
+
+    def load_from_json(self):
+        try:
+            with open(self.filename, "r") as fh:
+                data = json.load(fh)
+                if not data:
+                    return("The JSON file is empty.")
+                else:
+                    self.data.clear()
+                    for item in data:
+                        record = Record(item['name'])
+                        for phone in item['phones']:
+                            record.add_phone(phone)
+                        record.birthday = Birthday(item['birthday'])
+                        self.add_record(record)
+        except FileNotFoundError:
+            return("File not found. Creating a new address book.")
